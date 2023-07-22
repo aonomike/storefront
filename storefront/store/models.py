540d3d368ae6c707c00bcd2d44dcea5097407892
aonomike <mike.aono@wfp.org>
2023-07-22 21:46:26 +0300

    Change model meta and create index fro customer

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -6,13 +6,20 @@ from security.models import SecurityBaseModel
 
 class Collection(SecurityBaseModel):
     name = models.CharField(max_length=255, blank=False, null=False)
-    featured_product = models.ForeignKey("Product",null=True, on_delete=models.SET_NULL, related_name="collection_product")
+    featured_product = models.ForeignKey(
+        "Product",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="collection_product",
+    )
+
     class Meta:
         verbose_name = "Collection"
         verbose_name_plural = "Collections"
 
     def __str__(self):
         return self.name
+
 
 # Create your models here.
 class Product(SecurityBaseModel):
@@ -27,10 +34,11 @@ class Product(SecurityBaseModel):
     promotions = models.ManyToManyField(
         "Promotion",
     )
+
     class Meta:
         verbose_name = "Product"
         verbose_name_plural = "Products"
-        
+
     def __str__(self) -> str:
         return self.title
 
@@ -47,14 +55,17 @@ class Order(SecurityBaseModel):
     payment_status = models.CharField(
         max_length=20, choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING
     )
-    customer = models.ForeignKey("Customer", on_delete=models.PROTECT,related_name="order_customer")
+    customer = models.ForeignKey(
+        "Customer", on_delete=models.PROTECT, related_name="order_customer"
+    )
 
     class Meta:
         verbose_name = "Order"
         verbose_name_plural = "Orders"
 
     def __str__(self):
-        return f"{self.customer.pk} {self.customer.first_name} {self.payment_status}" 
+        return f"{self.customer.pk} {self.customer.first_name} {self.payment_status}"
+
 
 class Customer(SecurityBaseModel):
     MEMBERSHIP_SILVER = 1
@@ -77,41 +88,65 @@ class Customer(SecurityBaseModel):
     class Meta:
         verbose_name = "Customer"
         verbose_name_plural = "Customers"
+        indexes = [models.Index(fields=["last_name", "first_name", "email"])]
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}" 
+        return f"{self.first_name} {self.last_name}"
 
 
 class Address(SecurityBaseModel):
     city = models.CharField(max_length=255)
     street = models.CharField(max_length=255)
     customer = models.OneToOneField(
-        Customer, on_delete=models.CASCADE, primary_key=True, related_name="address_customer"
+        Customer,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="address_customer",
     )
     zip = models.CharField(max_length=255, null=True)
 
     class Meta:
-        verbose_name = "Addres"
+        verbose_name = "Address"
         verbose_name_plural = "Addresses"
 
     def __str__(self):
-        return f"{self.street} {self.city}" 
+        return f"{self.street} {self.city}"
+
+
 class OrderItem(SecurityBaseModel):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="order_item_product")
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name="order_item_product"
+    )
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
+    class Meta:
+        verbose_name = "OrderItem"
+        verbose_name_plural = "OrderItems"
+
 
 class Cart(SecurityBaseModel):
-    pass
+    class Meta:
+        verbose_name = "Cart"
+        verbose_name_plural = "Carts"
 
 
 class CartItem(SecurityBaseModel):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="cart_item_product")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="cart_item_product"
+    )
+
+    class Meta:
+        verbose_name = "CartItem"
+        verbose_name_plural = "CartItems"
 
 
 class Promotion(SecurityBaseModel):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
+
+    class Meta:
+        verbose_name = "Promotion"
+        verbose_name_plural = "Promotions"
